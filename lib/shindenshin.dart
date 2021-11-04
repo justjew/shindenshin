177cb3080c5eb1fd3aa7cab7d6c264c859463a85
justjew <justjew1406@gmail.com>
2021-11-05 01:23:33 +0200

    Fixed main file

--- lib/shindenshin.dart
+++ lib/shindenshin.dart
@@ -1,7 +1,9 @@
 library shindenshin;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/api_client.dart';
+export 'src/base_model.dart';
+export 'src/base_repo.dart';
+export 'src/base_store.dart';
+export 'src/pagination.dart';
+export 'src/subscriptable.dart';
+export 'src/websocket.dart';
